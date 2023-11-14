05b1651b2fd433b1e1e52a7f5590b8c5fcd64ac8
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-11-14 18:43:40 +0300

    feat: add typography

---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,30 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { Link } from 'react-router-dom'
+
+import s from './typography.module.scss'
+
+export type TypographyProps<T extends ElementType = 'button' | typeof Link> = {
+  as?: T
+  className: string
+  variant?:
+    | 'body1'
+    | 'body2'
+    | 'caption'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'large'
+    | 'link1'
+    | 'link2'
+    | 'overline'
+    | 'subtitle1'
+    | 'subtitle2'
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'div'>(
+  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
+) => {
+  const { as: Component = 'span', className, variant = 'body1', ...rest } = props
+
+  return <Component className={`${s[variant]} ${className}`} {...rest} />
+}
