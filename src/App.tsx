1244df3065d45a8bd5292af55ae4f8260dfa43a3
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-11-26 16:24:49 +0300

    feat: add table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
 import { LoginForm } from '@/components/auth/login-form/login-form'
+import {Table} from "@/components/ui/table";
+
+const columnOrder = ['title', 'cardsCount', 'updated', 'createdBy']
+
+const data = [
+  {
+    cardsCount: 100,
+    createdBy: 'Илья Кузнецов',
+    figma: 3,
+    title: 'Pack Name A',
+    updated: '2023-07-07',
+  },
+  {
+    cardsCount: 23,
+    createdBy: 'Анастасия Смирнова',
+    figma: 3,
+    title: 'Pack Name B',
+    updated: '2023-07-06',
+  },
+  {
+    cardsCount: 8,
+    createdBy: 'Дмитрий Петров',
+    figma: 3,
+    title: 'Pack Name C',
+    updated: '2023-07-05',
+  },
+  {
+    cardsCount: 6,
+    createdBy: 'Екатерина Иванова',
+    figma: 3,
+    title: 'Pack Name D',
+    updated: '2023-07-07',
+  },
+  {
+    cardsCount: 24,
+    createdBy: 'Александр Федоров',
+    figma: 3,
+    title: 'Pack Name E',
+    updated: '2023-07-04',
+  },
+]
 
 export const App = () => {
   return (
-    <div className={'wrapper'}>
+    <div className={'wrapper'} >
       <div className={'header'}>
         <div className={'header__container'}>
           <div className={'header__block-left'}>
@@ -44,6 +85,11 @@ export const App = () => {
       <div className={'form'}>
         <div className={'form__container'}>
           <LoginForm />
+        </div>
+      </div>
+      <div className={'table'}>
+        <div className="table__container">
+          <Table data={data} columnOrder={columnOrder} />
         </div>
       </div>
     </div>
