5844d06e9a78bf97f72a1a6e8bc468794ca941cf
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-12-02 16:43:55 +0300

    feat: add validation zod

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,17 +1,33 @@
 import { useController, useForm } from 'react-hook-form'
 
-import { Button } from '../../ui/button'
-import { Checkbox } from '../../ui/checkbox'
-import { TextField } from '../../ui/text-field'
+import { Checkbox, TextField } from '@/components/ui'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
-type FormValues = {
-  email: string
-  password: string
-  rememberMe: boolean
-}
+import { Button } from '../../ui/button'
+
+const loginSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(3, 'Too short password bitch '),
+  rememberMe: z.boolean().optional(),
+})
+
+type FormValues = z.infer<typeof loginSchema>
 
 export const LoginForm = () => {
-  const { control, handleSubmit, register } = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    resolver: zodResolver(loginSchema),
+  })
 
   const onSubmit = (data: FormValues) => {
     console.log(data)
@@ -21,14 +37,17 @@ export const LoginForm = () => {
     field: { onChange, value },
   } = useController({
     control,
-    defaultValue: false,
     name: 'rememberMe',
   })
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField {...register('email')} label={'email'} />
-      <TextField {...register('password')} label={'password'} />
+      <TextField {...register('email')} errorMessage={errors.email?.message} label={'email'} />
+      <TextField
+        {...register('password')}
+        errorMessage={errors.password?.message}
+        label={'password'}
+      />
       <Checkbox checked={value} label={'remember me'} onChange={onChange} />
       <Button type={'submit'}>Submit</Button>
     </form>
